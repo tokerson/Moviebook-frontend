db2ffe8d143d185176c1360521edee5cef9ede1c
tokerson <kuba.tokarzewski@gmail.com>
2018-12-20 19:41:29 +0100

    searching doesn't care about upper or lower case

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -11,7 +11,8 @@ class MovieList extends Component {
     
     this.state = {
       movies : JSON,
-      filtered : []
+      filtered : [],
+      searching : false
     }
   }
 
@@ -19,24 +20,30 @@ class MovieList extends Component {
   //this function filters json, looking for a title corresponding to given word
   filterMovies = (event) => {
     let filtered = this.state.movies.filter(item => {
-      return item.title.indexOf(event.target.value) > -1;
+      //toUpperCase, so user doesn't have to bother about Upper or Lower case sensitive titles.
+      return item.title.toUpperCase().indexOf(event.target.value.toUpperCase()) > -1;
     });
 
+
     this.setState({
-      filtered: filtered
+      filtered: filtered,
+      searching:  event.target.value.length > 0 
     });
   }
 
   render() {
 
     //we want to display either whole list of movies or filtered list depending on the users input
-    const movies = this.state.filtered.length === 0 ? this.state.movies : this.state.filtered
-
+    let movies = this.state.filtered.length === 0 ? this.state.movies : this.state.filtered;
+    if(this.state.filtered.length === 0 && this.state.searching === true) {
+      movies = []
+    }
+    
     return (
       <div>
           
           <input type="text" onChange={this.filterMovies} placeholder="Search"></input>
-          <MovieListItem movies={movies}/>          
+          <MovieListItem key={movies.id} movies={movies}/>          
       </div>
     );
   }
--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,6 +1,5 @@
-import React , { Component } from 'react';
+import React  from 'react';
 import {NavLink} from 'react-router-dom'
-import styles from '../css/App.css';
 
 const MovieListItem = (props) => {
 
