03c9d02fce7e32f720142b03f7124eba8b5b79ea
tokerson <kuba.tokarzewski@gmail.com>
2019-01-20 12:18:59 +0100

    added Dialog after succesfully adding new artist

--- src/components/AddArtistComponent.js
+++ src/components/AddArtistComponent.js
@@ -5,26 +5,49 @@ import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
 import '../css/AddMovieForm.css';
 import Axios from 'axios';
+import { Dialog, DialogTitle, DialogContent } from '@material-ui/core';
+
 
 const URL = 'http://localhost:8080';
 
 class AddArtistComponent extends Component {
 
     state={
-        picture:''
+        picture:'',
+        open: false,
+        success: false
     }
 
     handleSubmit= (event)=>{
         event.preventDefault();
 
         const data = new FormData(event.target);
+        try {
         let url = encodeURIComponent(data.get('pictureUrl'));
         var changed = url.replace(/%/g, "_");
+        } catch (err) {
+            this.setState({
+                success : false,
+                open: true
+            })
+            return;
+        }
         
         Axios.get(`${URL}/updateArtist/-1/${data.get('name')}/${data.get('surname')}/${data.get('origin')}/${data.get('date')}/${changed}`)
         .then(response => {
-            console.log(response);
+            let success = false;
+            if( response.data === "Successful"){
+                success = true;
+            }
+            this.setState({
+                success: true,
+                open: true
+            })
         })
+        .catch(err => this.setState({
+            success : false,
+            open: true
+        }))
     }
 
     handlePictureChange = (event) => {
@@ -33,8 +56,17 @@ class AddArtistComponent extends Component {
         })
     }
 
+    onDialogClose = () => {
+        this.setState({
+            open: false
+        })
+    }
+
     formRender = () => {
         return <div className="pictureWithForm">
+            <Dialog open={this.state.open} onClose={this.onDialogClose}> 
+            { this.state.success ? <DialogTitle>You successfuly added new artist</DialogTitle> : <DialogTitle>Operation Unsuccessful</DialogTitle>}
+                </Dialog>
             <div className="pictureWrapper">
                 <img className="picture" src={this.state.picture} alt=""></img>
             </div>
