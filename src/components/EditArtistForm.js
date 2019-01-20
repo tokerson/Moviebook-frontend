1357609228c61a78596e3b5fb6039d8f03bf60af
tokerson <kuba.tokarzewski@gmail.com>
2019-01-20 15:39:16 +0100

    edit artist works

--- src/components/EditArtistForm.js
+++ src/components/EditArtistForm.js
@@ -7,19 +7,51 @@ import '../css/AddMovieForm.css';
 import Axios from 'axios';
 import { Dialog, DialogTitle } from '@material-ui/core';
 
-
 const URL = 'http://localhost:8080';
 
 class EditArtistForm extends Component {
 
-    state={
-        picture:'',
-        open: false,
-        success: false
+    state = {
+        surname: "",
+        name: "",
+        origin: "",
+        birthDate: "",
+        pictureUrl: "",
+        types: [],
+        prizes: [],
+        films: [],
+        open : false,
+        success : false,
     }
 
+
     componentWillMount() {
-        // Axios.get(`${URL}/getArtist/${this.props.match.params.id}`)
+        const id = this.props.match.params.id
+        this.getArtist(id);
+    }
+
+    getArtist(id) {
+        Axios.get(`${URL}/getArtist/${id}`)
+        .then(response => {
+            const surnameResult = response.data.surname;
+            const nameResult = response.data.name;
+            const originResult = response.data.origin;
+            const birthDateResult = response.data.birthDate;
+            const pictureUrlResult = response.data.pictureUrl;
+            const typesResult = response.data.types;
+            const prizesResult = response.data.prizes;
+            const filmsResult = response.data.typeAndFilms;
+            this.setState({
+                surname: surnameResult,
+                name: nameResult,
+                origin: originResult,
+                birthDate: birthDateResult,
+                pictureUrl: pictureUrlResult,
+                types: typesResult,
+                prizes: prizesResult,
+                films: filmsResult
+            });
+        })
     }
 
     handleSubmit= (event)=>{
@@ -37,14 +69,14 @@ class EditArtistForm extends Component {
             return;
         }
         
-        Axios.get(`${URL}/updateArtist/-1/${data.get('name')}/${data.get('surname')}/${data.get('origin')}/${data.get('date')}/${changed}`)
+        Axios.get(`${URL}/updateArtist/${this.props.match.params.id}/${data.get('name')}/${data.get('surname')}/${data.get('origin')}/${data.get('date')}/${changed}`)
         .then(response => {
             let success = false;
             if( response.data === "Successful"){
                 success = true;
             }
             this.setState({
-                success: true,
+                success: success,
                 open: true
             })
         })
@@ -54,35 +86,35 @@ class EditArtistForm extends Component {
         }))
     }
 
-    handlePictureChange = (event) => {
-        this.setState({
-            picture: event.target.value
-        })
-    }
-
     onDialogClose = () => {
         this.setState({
             open: false
         })
     }
 
+    handleChooseMovie = (movies) => {
+        this.setState({
+            movies : movies
+        })
+    }
+
     formRender = () => {
         return <div className="pictureWithForm">
             <Dialog open={this.state.open} onClose={this.onDialogClose}> 
-            { this.state.success ? <DialogTitle>You successfuly added new artist</DialogTitle> : <DialogTitle>Operation Unsuccessful</DialogTitle>}
+            { this.state.success ? <DialogTitle>You successfuly edited this artist</DialogTitle> : <DialogTitle>Operation Unsuccessful</DialogTitle>}
                 </Dialog>
             <div className="pictureWrapper">
-                <img className="picture" src={this.state.picture} alt=""></img>
+                <img className="picture" src={this.state.pictureUrl} alt=""></img>
             </div>
             <form onSubmit={this.handleSubmit} className="movieForm">
-                <TextField type="text" id="name" name="name" label="Name" variant="outlined"  margin="dense"></TextField>
-                <TextField type="text" id="surname" name="surname" label="Surname" variant="outlined"  margin="dense"></TextField>
-                <TextField type="text" id="origin" name="origin" label="Origin" variant="outlined"  margin="dense"></TextField>
-                <TextField type="date" id="date" name="date" label="Date of birth" InputLabelProps={{
+                <TextField type="text" id="name" name="name" value={this.state.name} label="Name" variant="outlined" onChange={(event)=>{this.setState({name:event.target.value})}} margin="dense"></TextField>
+                <TextField type="text" id="surname" name="surname" value={this.state.surname} label="Surname" variant="outlined" onChange={(event)=>{this.setState({surname:event.target.value})}} margin="dense"></TextField>
+                <TextField type="text" id="origin" name="origin" value={this.state.origin}label="Origin" variant="outlined" onChange={(event)=>{this.setState({origin:event.target.value})}} margin="dense"></TextField>
+                <TextField type="date" id="date" name="date" value={this.state.birthDate} label="Date of birth" InputLabelProps={{
                     shrink: true,
-                }} variant="outlined" margin="dense"></TextField>
-                <TextField type="text" id="pictureUrl" multiline={true} name="pictureUrl" label="URL of picture" variant="outlined" margin="dense" onChange={this.handlePictureChange}></TextField>
-                <Button variant="outlined"type="submit" margin="dense">Add Artist</Button>
+                }} variant="outlined" onChange={(event)=>{this.setState({birthDate:event.target.value})}} margin="dense"></TextField>
+                <TextField type="text" id="pictureUrl" value={this.state.pictureUrl} multiline={true} name="pictureUrl" label="URL of picture" variant="outlined" onChange={(event)=>{this.setState({pictureUrl:event.target.value})}} margin="dense" ></TextField>
+                <Button variant="outlined"type="submit" margin="dense">Edit Artist</Button>
             </form>
         </div>
     }
